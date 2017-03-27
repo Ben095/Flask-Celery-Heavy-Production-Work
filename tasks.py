4e6e8592f7933cd9464af3caeefacbac4668bf68
Ben095 <dahalbiplovechs@gmail.com>
2017-03-26 19:09:54 -0700

    fixed

--- tasks.py
+++ tasks.py
@@ -394,7 +394,7 @@ def InstagramMain(name):
             response = s.get('https://www.instagram.com/'+str(name)+'/?__a=1').text
             JSON = json.loads(response)
             UID = JSON['user']['id']
-            ig = InstagramAPI("OutReachTest", "outreach1234")
+            ig = InstagramAPI("outreach1998", "4108605$Biplov")
             ig.login()
             main_list = ig.getTotalFollowersID2(UID)
             finalArr = []
